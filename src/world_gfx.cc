d216276d5cb3b67e4d23a2b056a23e685f034c18
Maciek Borzecki <maciek.borzecki@gmail.com>
2009-11-14 13:17:57 +0100

    cleanups (possible to build)

---
+++ src/world_gfx.cc
@@ -0,0 +1,18 @@
+#include "world_gfx.h"
+
+world_gfx::world_gfx()
+{
+
+}
+
+world_gfx::~world_gfx()
+{
+
+}
+
+void
+world_gfx::draw(sys::gfx * gfx)
+{
+
+}
+
---
+++ src/world_phys.cc
@@ -0,0 +1,18 @@
+#include "world_phys.h"
+
+world_phys::world_phys()
+{
+}
+
+world_phys::~world_phys()
+{
+
+}
+
+void
+world_phys::advance(double delta_t)
+{
+
+}
+
+
